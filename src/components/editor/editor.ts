66aef9d4c6eeef906c6947439438cdd7eb5a6e22
-A- <1157988103@qq.com>
2017-12-03 11:26:31 +0800

    add line numbers in editor component

--- src/components/editor/editor.ts
+++ src/components/editor/editor.ts
@@ -5,5 +5,22 @@ import Component from 'vue-class-component';
   template: require('./editor.html'),
 })
 export class EditorComponent extends Vue {
-
+  lines: number = 1;
+  private addLines (event: Event) {
+    let textarea = this.$el.querySelector('textarea');
+    let match = textarea.value.match(/\n/g);
+    this.lines = match ? match.length + 1 : this.lines;
+    this.$nextTick(function() {
+      let div = this.$el.querySelector('.lidis-editor-line__scroll');
+      let p = div.querySelector('p');
+      div.scrollTo(0, textarea.scrollTop);
+    });
+  }
+  private scrollChange() {
+    this.$nextTick(function() {
+      let textarea = this.$el.querySelector('textarea');
+      let div = this.$el.querySelector('.lidis-editor-line__scroll');
+      div.scrollTo(0, textarea.scrollTop);
+    });
+  }
 }
