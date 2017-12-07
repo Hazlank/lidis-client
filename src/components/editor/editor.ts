ae1c255f5c878b6ee5ff93dc3a17bf41ed6636e6
妙蛙种子 <foru.fy@gmail.com>
2017-12-07 21:35:37 +0800

    continue work
    * tweak list component
    * add prettier config

--- src/components/editor/editor.ts
+++ src/components/editor/editor.ts
@@ -1,27 +1,32 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import debounce from 'lodash.debounce';
 
 @Component({
-  template: require('./editor.html'),
+  template: require('./editor.html')
 })
 export class EditorComponent extends Vue {
-
   private numberOfRows: number = 1;
 
-  private handleInput (event: Event) {
+  private handleInput(event: Event) {
     const editor = this.$refs.editor as HTMLTextAreaElement;
     const matchResult = editor.value.match(/\n/g) || '';
     this.numberOfRows = matchResult.length + 1;
     this.scrollToTop();
   }
 
-  private scrollChange() {
-    this.scrollToTop();
-  }
-
   private async scrollToTop() {
     await this.$nextTick();
-    const { countContainer: container, editor } = this.$refs;
-    (container as HTMLElement).scrollTo(0, (editor as HTMLTextAreaElement).scrollTop);
+    const { countContainer: container, editor } = this.$refs as {
+      countContainer: HTMLElement;
+      editor: HTMLTextAreaElement;
+    };
+    container.scrollTo(0, editor.scrollTop);
+  }
+
+  private mounted() {
+    const editor = this.$refs.editor as Element;
+    editor.addEventListener('scroll', debounce(this.scrollToTop));
+    editor.addEventListener('input', debounce(this.handleInput));
   }
 }
